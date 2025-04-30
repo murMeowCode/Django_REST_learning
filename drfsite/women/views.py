6c4c88b2e716edd19e22bc186d8e3c6bf32f0c67
murmeowcode <grisha23.01.2004@gmail.com>
2025-04-30 16:44:28 +0300

    lesson 4 serial

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -6,13 +6,16 @@ from .models import Women
 
 class WomenAPIView(APIView):
     def get(self, request):
-        lst = Women.objects.all().values_list()
-        return Response({'posts':lst})
+        lst = Women.objects.all()
+        return Response({'posts':WomenSerializer(lst,many = True).data})
     
     def post(self, request):
+        serializer = WomenSerializer(request.data)
+        serializer.is_valid(raise_exception=True)
+        
         post_new = Women.objects.create(
             title = request.data['title'],
             content = request.data['content'],
             cat_id = request.data['cat_id']
         )
-        return Response({'post':model_to_dict(post_new)})
+        return Response({'post':WomenSerializer(post_new).data})
