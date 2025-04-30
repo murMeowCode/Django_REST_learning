5b4e0a2848c2a7596b3add3663ea857455649c80
murmeowcode <grisha23.01.2004@gmail.com>
2025-04-30 15:46:28 +0300

    basic APIView

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
-from rest_framework import generics
+from django.forms import model_to_dict
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .serializers import WomenSerializer
 from .models import Women
 
-class WomenAPIView(generics.ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+class WomenAPIView(APIView):
+    def get(self, request):
+        lst = Women.objects.all().values_list()
+        return Response({'posts':lst})
+    
+    def post(self, request):
+        post_new = Women.objects.create(
+            title = request.data['title'],
+            content = request.data['content'],
+            cat_id = request.data['cat_id']
+        )
+        return Response({'post':model_to_dict(post_new)})
