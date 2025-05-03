207f308b767cfe456f2c4569da74f2bc6bd66fa9
murmeowcode <grisha23.01.2004@gmail.com>
2025-05-03 20:20:22 +0300

    upd,create and save lesson 5

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,5 +1,6 @@
 from django.forms import model_to_dict
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.views import APIView
 from .serializers import WomenSerializer
 from .models import Women
@@ -10,12 +11,22 @@ class WomenAPIView(APIView):
         return Response({'posts':WomenSerializer(lst,many = True).data})
     
     def post(self, request):
-        serializer = WomenSerializer(request.data)
+        serializer = WomenSerializer(data = request.data)
         serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'post':serializer.data})
+    
+    def put(self,requset,*args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:
+            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
         
-        post_new = Women.objects.create(
-            title = request.data['title'],
-            content = request.data['content'],
-            cat_id = request.data['cat_id']
-        )
-        return Response({'post':WomenSerializer(post_new).data})
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = WomenSerializer(data=requset.data,instance = instance)
+        serializer.is_valid()
+        serializer.save()
+        return Response(status=status.HTTP_200_OK)
