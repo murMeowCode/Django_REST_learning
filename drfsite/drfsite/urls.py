276e7ea266225fb607d258e5a3b9757005f1bce3
murmeowcode <grisha23.01.2004@gmail.com>
2025-05-05 20:24:14 +0300

    lesson 9 improve viewset

--- drfsite/drfsite/urls.py
+++ drfsite/drfsite/urls.py
@@ -15,17 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from women.views import *
-
+from django.urls import include, path
 from rest_framework import routers
+from women.views import WomenViewSet
 
-router = routers.SimpleRouter()
-router.register(r'women', WomenViewSet)
+router = routers.DefaultRouter()
+router.register(r'women', WomenViewSet,basename='women')
+print(router.urls)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',include(router.urls))
-    # path('api/v1/womenlist/',WomenViewSet.as_view({'get':'list'})),
-    # path('api/v1/womenlist/<int:pk>/',WomenViewSet.as_view({'put':'update'})),
 ]
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,25 +1,38 @@
-from django.forms import model_to_dict
+from rest_framework import viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework import status,generics, viewsets
-from rest_framework.views import APIView
+
+from .models import Category, Women
 from .serializers import WomenSerializer
-from .models import Women
 
 
-
-# class WomenAPIList(generics.ListCreateAPIView):
-#     queryset = Women.objects.all()
-#     serializer_class = WomenSerializer
-
-# class WomenAPIUpdate(generics.UpdateAPIView):
-#     queryset = Women.objects.all()
-#     serializer_class = WomenSerializer
-    
-# class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Women.objects.all()
-#     serializer_class = WomenSerializer
-    
-    
 class WomenViewSet(viewsets.ModelViewSet):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer    
+    """_summary_
+
+    Args:
+        viewsets (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    serializer_class = WomenSerializer
+    
+    def get_queryset(self):
+        pk = self.kwargs.get("pk")
+        if not pk:
+            return Women.objects.all()[:3]
+        else:
+            return Women.objects.filter(pk = pk)
+    
+    @action(methods = ['get'], detail = True)
+    def category(self, request, pk = None):
+        """_summary_
+
+        Args:
+            request (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats':cats.name})
