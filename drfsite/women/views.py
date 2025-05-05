a5dbce7afd0549fc251d82fe3cde758e5db5f24a
murmeowcode <grisha23.01.2004@gmail.com>
2025-05-05 12:59:54 +0300

    lesson 7 RUD API

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -10,29 +10,38 @@ import logging
 class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
+
+class WomenAPIUpdate(generics.UpdateAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
     
-class WomenAPIView(APIView):
-    def get(self, request):
-        lst = Women.objects.all()
-        return Response({'posts':WomenSerializer(lst,many = True).data})
+class WomenAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
     
-    def post(self, request):
-        serializer = WomenSerializer(data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response({'post':serializer.data})
     
-    def put(self,requset,*args, **kwargs):
-        pk = kwargs.get("pk", None)
-        if not pk:
-            return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+# class WomenAPIView(APIView):
+#     def get(self, request):
+#         lst = Women.objects.all()
+#         return Response({'posts':WomenSerializer(lst,many = True).data})
+    
+#     def post(self, request):
+#         serializer = WomenSerializer(data = request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response({'post':serializer.data})
+    
+#     def put(self,requset,*args, **kwargs):
+#         pk = kwargs.get("pk", None)
+#         if not pk:
+#             return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
         
-        try:
-            instance = Women.objects.get(pk=pk)
-        except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+#         try:
+#             instance = Women.objects.get(pk=pk)
+#         except:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
         
-        serializer = WomenSerializer(data=requset.data,instance = instance)
-        serializer.is_valid()
-        serializer.save()
-        return Response(status=status.HTTP_200_OK)
+#         serializer = WomenSerializer(data=requset.data,instance = instance)
+#         serializer.is_valid()
+#         serializer.save()
+#         return Response(status=status.HTTP_200_OK)
